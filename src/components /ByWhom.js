adb8c3fdcc4d3079cc8582b99060d4d1e5c18e49
Rachel Kelly <racheldk126@gmail.com>
2022-10-18 08:54:17 -0700

    artwork info request

--- src/components /ByWhom.js
+++ src/components /ByWhom.js
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
-import { toBeEmptyDOMElement } from "@testing-library/jest-dom/dist/matchers";
-import { Navigate, useNavigate } from "react-router";
 import ImagesByWhom from "./ImagesByWhom";
 
 export default function ByWhom({artworkIDList, setArtworkIDList, setQuizTitle, quizTitle}) {
@@ -46,37 +44,7 @@ export default function ByWhom({artworkIDList, setArtworkIDList, setQuizTitle, q
             });
         console.log(worksIDs);
         setArtworkIDList(worksIDs);
-        // axios
-        //     .get(
-        //         "https://api.artic.edu/api/v1/artworks/87467?fields=id,artist_title,title,artwork_type_display,date_display,image_id,is_public_domain,thumbnail,material_titles",
-        //         {}
-        //     )
-        //     .then((res) => {
-        //         console.log(res.data.data);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //         alert(error.message);
-        //     });
-    },[]);
-
-    const getImages = (worksIDs) => {
-        // map through allWorksList
-        console.log("get those images!");
-        console.log(worksIDs);
-        let requestList = [];
-        worksIDs.forEach((id) => console.log(id));
-        // worksIDs.forEach((element) => {
-        //     console.log(element);
-        //     requestList.push(
-        //         axios.get(
-        //             `https://api.artic.edu/api/v1/artworks/${element}?fields=id,artist_title,title,artwork_type_display,date_display,image_id,is_public_domain,thumbnail,material_titles`
-        //         )
-        //     );
-        // });
-        console.log(requestList);
-    };
-
+    },[setArtworkIDList]);
 
 
     return (
--- src/components /ImagesByWhom.js
+++ src/components /ImagesByWhom.js
@@ -1,18 +1,48 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function ImagesByWhom({artworkIDList, quizTitle}){
-    
-    console.log(artworkIDList)
-    console.log(quizTitle)
-    
-    useEffect(()=>{
-        
-    },[])
-    
-    return(
+export default function ImagesByWhom({ artworkIDList, quizTitle }) {
+    const [artworkList, setArtworkList] = useState([])
+
+    useEffect(() => {
+        let imageRequests = [];
+        let imageResponses = []
+        artworkIDList.forEach((id) => {
+            imageRequests.push(
+                axios.get(
+                    `https://api.artic.edu/api/v1/artworks/${id}?fields=id,artist_title,title,artwork_type_display,date_display,image_id,is_public_domain,thumbnail,material_titles,artist_id`,
+                    {}
+                )
+            );
+        });
+        console.log(...imageRequests);
+        axios.all(imageRequests)
+        .then(
+            axios.spread((...responses)=>{
+            // console.log(responses)
+            responses.forEach((response)=>{
+                console.log(response.data)
+                if((response.data.data.artist_id===35577||response.data.data.artist_id===35809)&&response.data.data.is_public_domain===true){
+                    imageResponses.push(response.data.data)
+                } 
+            })
+            console.log(imageResponses)
+            setArtworkList(imageResponses)
+        }))
+    }, []);
+
+    return (
         <>
-        <h2>Quiz</h2>
-        <h3>{quizTitle}</h3>
+            <h2>Quiz</h2>
+            <h3>{quizTitle}</h3>
+            {artworkList.map((artwork)=>
+            <div> 
+                {/* <p>Artwork id: {artwork.id}</p> */}
+                <p>Title: {artwork.title}</p>
+                <p>by: {artwork.artist_title}</p>
+                <p>public domain: {artwork.is_public_domain ? ("yes"):("NOOOOOOOO")}</p>
+            </div>
+            )}
         </>
-    )
+    );
 }
