7962d815657d61dcdcc2dd998845df05cb9e5449
Rachel Kelly <racheldk126@gmail.com>
2022-10-18 09:09:13 -0700

    images rendering on page, alt text pulled from API response

--- src/components /ImagesByWhom.js
+++ src/components /ImagesByWhom.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function ImagesByWhom({ artworkIDList, quizTitle }) {
-    const [artworkList, setArtworkList] = useState([])
+    const [artworkList, setArtworkList] = useState([]);
 
     useEffect(() => {
         let imageRequests = [];
-        let imageResponses = []
+        let imageResponses = [];
         artworkIDList.forEach((id) => {
             imageRequests.push(
                 axios.get(
@@ -16,35 +16,48 @@ export default function ImagesByWhom({ artworkIDList, quizTitle }) {
             );
         });
         console.log(...imageRequests);
-        axios.all(imageRequests)
-        .then(
-            axios.spread((...responses)=>{
-            // console.log(responses)
-            responses.forEach((response)=>{
-                console.log(response.data)
-                if((response.data.data.artist_id===35577||response.data.data.artist_id===35809)&&response.data.data.is_public_domain===true){
-                    imageResponses.push(response.data.data)
-                } 
+        axios.all(imageRequests).then(
+            axios.spread((...responses) => {
+                // console.log(responses)
+                responses.forEach((response) => {
+                    console.log(response.data);
+                    if (
+                        (response.data.data.artist_id === 35577 ||
+                            response.data.data.artist_id === 35809) &&
+                        response.data.data.is_public_domain === true
+                    ) {
+                        imageResponses.push(response.data.data);
+                    }
+                });
+                console.log(imageResponses);
+                const sortedImageResponses = imageResponses.sort((a, b) => {
+                    return a.id - b.id;
+                });
+                console.log(sortedImageResponses);
+                setArtworkList(sortedImageResponses);
             })
-            console.log(imageResponses)
-            const sortedImageResponses = imageResponses.sort((a,b)=>{return a.id-b.id})
-            console.log(sortedImageResponses)
-            setArtworkList(sortedImageResponses)
-        }))
+        );
     }, []);
 
     return (
         <>
             <h2>Quiz</h2>
             <h3>{quizTitle}</h3>
-            {artworkList.map((artwork)=>
-            <div> 
-                <p>Artwork id: {artwork.id}</p>
-                <p>Title: {artwork.title}</p>
-                <p>by: {artwork.artist_title}</p>
-                <p>public domain: {artwork.is_public_domain ? ("yes"):("NOOOOOOOO")}</p>
-            </div>
-            )}
+            {artworkList.map((artwork) => (
+                <div>
+                    <img
+                        src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+                        alt={artwork.thumbnail.alt_text}
+                    ></img>
+                    <p>Artwork id: {artwork.id}</p>
+                    <p>Title: {artwork.title}</p>
+                    <p>by: {artwork.artist_title}</p>
+                    <p>
+                        public domain:{" "}
+                        {artwork.is_public_domain ? "yes" : "NOOOOOOOO"}
+                    </p>
+                </div>
+            ))}
         </>
     );
 }
